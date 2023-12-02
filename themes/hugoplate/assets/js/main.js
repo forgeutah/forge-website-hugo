3265c77415859e9771da3213310fb654f842cf74
CB <clint@getkolla.com>
2023-12-01 18:27:36 -0700

    first commit

---
+++ themes/hugoplate/assets/js/main.js
@@ -0,0 +1,43 @@
+// main script
+(function () {
+  "use strict";
+
+  // Dropdown Menu Toggler For Mobile
+  // ----------------------------------------
+  const dropdownMenuToggler = document.querySelectorAll(
+    ".nav-dropdown > .nav-link",
+  );
+
+  dropdownMenuToggler.forEach((toggler) => {
+    toggler?.addEventListener("click", (e) => {
+      e.target.parentElement.classList.toggle("active");
+    });
+  });
+
+  // Testimonial Slider
+  // ----------------------------------------
+  new Swiper(".testimonial-slider", {
+    spaceBetween: 24,
+    loop: true,
+    pagination: {
+      el: ".testimonial-slider-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+    },
+  });
+})();
+
+  // Typwriter titles
+  // ----------------------------------------
+  var typed = new Typed('#typewriter', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 50,
+  });
